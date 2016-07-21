70612432c504e3acfd9b9d0c4311637caa30c0af
lirachel <rachel.li.325@gmail.com>
2016-07-21 13:37:58 -0500

    i think it works now

--- src/main.c
+++ src/main.c
@@ -87,7 +87,7 @@ layer_add_child(window_get_root_layer(window), s_battery_layer);
 	// Create temperature Layer
 //	GRect bounds = layer_get_bounds(layer);
 	
-	s_weather_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(125, 120), 100, 25));
+	s_weather_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(125, 120), 144, 50));
 	
 	// Style the text
 	text_layer_set_background_color(s_weather_layer, GColorClear);
@@ -96,7 +96,8 @@ layer_add_child(window_get_root_layer(window), s_battery_layer);
 	text_layer_set_text(s_weather_layer, "Loading...");
 	
 	// Create second custom font, apply it and add to Window
-	s_weather_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
+	s_weather_font = fonts_load_custom_font(
+                          resource_get_handle(RESOURCE_ID_GEOSANS_18));
 	text_layer_set_font(s_weather_layer, s_weather_font);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 
@@ -119,37 +120,96 @@ static void main_window_unload(Window *window) {
 
 //register all events
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
- update_time();
- 
+	update_time();
+	
+ 	// Get weather update every 30 minutes
+	if(tick_time->tm_min % 30 == 0) {
+	// Begin dictionary
+		DictionaryIterator *iter;
+		app_message_outbox_begin(&iter);
+
+	// Add a key-value pair
+	dict_write_uint8(iter, 0, 0);
+
+	// Send the message!
+	app_message_outbox_send();
+}
 }
 
-  
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+	// Store incoming information
+	static char temperature_buffer[8];
+	static char conditions_buffer[32];
+	static char weather_layer_buffer[32];
+	
+	// Read tuples for data
+	Tuple *temp_tuple = dict_find(iterator, MESSAGE_KEY_TEMPERATURE);
+	Tuple *conditions_tuple = dict_find(iterator, MESSAGE_KEY_CONDITIONS);
+
+	// If all data is available, use it
+	if(temp_tuple && conditions_tuple) {
+		snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+		snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+	}
+	
+	// Assemble full string and display
+	snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+	text_layer_set_text(s_weather_layer, weather_layer_buffer);
+
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
 
 
 static void init() {
  
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  // Register for battery level updates
-  battery_state_service_subscribe(battery_callback);
+	// Register with TickTimerService
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	// Register for battery level updates
+	battery_state_service_subscribe(battery_callback);
   
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
+	// Create main Window element and assign to pointer
+	s_main_window = window_create();
 
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+	// Set handlers to manage the elements inside the Window
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+     .load = main_window_load,
+     .unload = main_window_unload
+    });
 
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
+	// Show the Window on the watch, with animated=true
+	window_stack_push(s_main_window, true);
   
-  // Make sure the time is displayed from the start
-  update_time();
+	// Make sure the time is displayed from the start
+	update_time();
 	
 	// Ensure battery level is displayed from the start
-battery_callback(battery_state_service_peek());
+	battery_callback(battery_state_service_peek());
+	
+	// Register callbacks
+	app_message_register_inbox_received(inbox_received_callback);
+	app_message_register_inbox_dropped(inbox_dropped_callback);
+	app_message_register_outbox_failed(outbox_failed_callback);
+	app_message_register_outbox_sent(outbox_sent_callback);
+	
+	// Open AppMessage
+	const int inbox_size = 128;
+	const int outbox_size = 128;
+	app_message_open(inbox_size, outbox_size);
+
+
 
 }
 
